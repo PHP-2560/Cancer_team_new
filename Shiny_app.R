52354d48604226bd2d587f912a6c9294b73653ab
Maydelis <maydelis_minaya@brown.edu>
2018-12-05 15:34:44 -0500

    colon data added

--- Shiny_app.R
+++ Shiny_app.R
@@ -1,4 +1,15 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
-ui <- fluidPage()
-server <- function(input, output) {}
-shinyApp(ui = ui, server = server)
+library(maps)
+
+colon_data <- read.csv("colon_data.csv")
+print(str(colon_data))
+##
