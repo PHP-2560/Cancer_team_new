d58dbce9ec1e6c9ef4c00dd11b589157ee12c5ff
Maydelis <maydelis_minaya@brown.edu>
2018-12-05 15:56:56 -0500

    adding things

--- Shiny_app.R
+++ Shiny_app.R
@@ -1,3 +1,9 @@
+library(shiny)
+ui <- fluidPage()
+server <- function(input, output) {}
+shinyApp(ui = ui, server = server)
+
+
 #
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
@@ -7,9 +13,41 @@
 #    http://shiny.rstudio.com/
 #
 
+# Setting up packages
 library(shiny)
+library(ggplot2)
+library(ggmap)
 library(maps)
+library(mapdata)
+library(dplyr)
+
 
 colon_data <- read.csv("colon_data.csv")
 print(str(colon_data))
-##
+
+ui <- fluidPage(
+  titlePanel("Colon Cancer Rates in the United States 2011-2015"),
+  sidebarLayout(
+    sidebarPanel("our inputs will go here"),
+    mainPanel("the results will go here")
+  )
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+server <- function(input, output) {}
+
+shinyApp(ui = ui, server = server)
